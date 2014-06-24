9f41591db711c64c416ca8481a88250e610e8a54
NatureZhang <895234387@qq.com>
2014-06-24 14:27:38 +0800

    测试
    这是一个测试

---
+++ CocoaPodsDemo/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  CocoaPodsDemo
+//
+//  Created by lanou3g on 14-6-23.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ CocoaPodsDemo/RootViewController.m
@@ -0,0 +1,39 @@
+//
+//  RootViewController.m
+//  CocoaPodsDemo
+//
+//  Created by lanou3g on 14-6-23.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import "RootViewController.h"
+#import <AFNetworking.h>
+#import <JSONKit.h>
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
